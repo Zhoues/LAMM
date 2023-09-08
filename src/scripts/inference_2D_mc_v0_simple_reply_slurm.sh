06e1024fe3dc4f75b510aa2a9e6618fc16dc848b
Zhoues <2868470542@qq.com>
2023-09-08 19:07:24 +0800

    feat: update benchmark scripts

---
+++ src/scripts/inference_2D_mc_v0_simple_reply_slurm.sh
@@ -0,0 +1,34 @@
+# partition=AI4Good_X
+
+partition=$1
+dataset=minecraft
+
+exp=LAMM_MineClip_36k_simple_reply_Vicuna_13b_v0
+
+base_data_path=../datasets/2D_Benchmark
+token_num=196
+layer=-2
+answerdir=../answers
+mkdir -p ${answerdir}/${exp}
+results_path=../results
+mkdir -p ${results_path}/${exp}
+
+srun -p ${partition} --gres=gpu:1 --ntasks-per-node=1 --kill-on-bad-exit \
+    python inference_2d.py \
+        --model lamm_peft \
+        --encoder_pretrain mineclip \
+        --llm_ckpt_path ../model_zoo/vicuna_ckpt/13b_v0 \
+        --encoder_ckpt_path ../model_zoo/mineclip_ckpt/mineclip_image_encoder_vit-B_196tokens.pth \
+        --delta_ckpt_path ../ckpt/${exp}/pytorch_model.pt \
+        --max_tgt_len 400 \
+        --lora_r 32 \
+        --lora_alpha 32 \
+        --lora_dropout 0.1 \
+        --num_vision_token ${token_num} \
+        --vision_output_layer ${layer} \
+        --conv_mode default \
+        --dataset-name ${dataset} \
+        --base-data-path ${base_data_path} \
+        --inference-mode common \
+        --bs 32 \
+        --answers-dir ${answerdir}/${exp} \
