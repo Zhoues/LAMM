266f2da02d2c8ea3fc382c84eea57d8435b785be
Zhoues <2868470542@qq.com>
2024-05-31 15:28:06 +0800

    chore: update scripts

--- src/scripts/mllm_api_slurm.sh
+++ src/scripts/mllm_api_slurm.sh
@@ -1,5 +1,5 @@
-# partition=AI4Good_X
+# partition=AI4Good_S
 partition=$1
    
 srun -p ${partition} --gres=gpu:1 --ntasks-per-node=1 --kill-on-bad-exit \
-    python mllm_api_v1.5.py \
+    python mllm_api_mc_v1.5.py \
